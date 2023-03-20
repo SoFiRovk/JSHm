03062e774582d62af12f5c944327a3a8cd9ce5a8
SonyaR <Sonya@mail.ru>
2023-03-20 18:31:00 +0800

    initial commit

---
+++ 10/10/data.js
@@ -0,0 +1,65 @@
+const users = `
+{
+    "results": [
+      {
+        "gender": "male",
+        "name": {
+          "title": "Mr",
+          "first": "Justin",
+          "last": "Simmons"
+        },
+        "location": {
+          "street": {
+            "number": 2602,
+            "name": "Church Lane"
+          },
+          "city": "Castlebar",
+          "state": "Cork",
+          "country": "Ireland",
+          "postcode": 82880,
+          "coordinates": {
+            "latitude": "-2.3370",
+            "longitude": "-141.2603"
+          },
+          "timezone": {
+            "offset": "-11:00",
+            "description": "Midway Island, Samoa"
+          }
+        },
+        "email": "justin.simmons@example.com",
+        "login": {
+          "uuid": "9d04031c-9743-41e6-afcb-32480a879d3a",
+          "username": "smallpeacock320",
+          "password": "wolfgang",
+          "salt": "KBroh4y6",
+          "md5": "22f5ef330e7aa61ad883f436be9567fd",
+          "sha1": "259d61d02b1ba9e191488902fcde77ecc54d225d",
+          "sha256": "ae4b2998bd835361f4fc31aa2159246efd2d9f8b8d4991b598d014f0c54e844e"
+        },
+        "dob": {
+          "date": "1954-08-04T04:27:28.582Z",
+          "age": 68
+        },
+        "registered": {
+          "date": "2007-10-07T16:23:11.604Z",
+          "age": 15
+        },
+        "phone": "011-654-5826",
+        "cell": "081-510-9509",
+        "id": {
+          "name": "PPS",
+          "value": "4193053T"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/54.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/54.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/54.jpg"
+        },
+        "nat": "IE"
+      }
+    ],
+    "info": {
+        "seed": "bc713a75bd95b693",
+        "results": 1,
+        "page": 1,
+        "version": "1.4"}}`;
---
+++ 10/10/main.js
@@ -0,0 +1,42 @@
+const userData = JSON.parse(users);
+// const imgEl = document.querySelector(".user_img");
+// imgEl.src = userData.results[0].picture.medium;
+
+// const user_FirstNameEl = document.querySelector(".user_FirstName");
+// user_FirstNameEl.textContent = userData.results[0].name.first;
+
+// const user_LastNameEl = document.querySelector(".user_LastNameEl");
+// user_LastNameEl.textContent = userData.results[0].name.first;
+
+// const userCountryEl = document.querySelector(".userCountry");
+// userCountryEl.textContent = userData.results[0].name.first;
+
+// const userEmailEl = document.querySelector(".userEmail");
+// userEmailEl.textContent = userData.results[0].email;
+
+// const userPhoneEl = document.querySelector(".userPhone");
+// userPhoneEl.textContent = userData.results[0].location;
+
+const contentEl = document.querySelector(".content");
+userData.results.array.forEach((el) => {
+  console.log(el);
+  const userWrapEl = document.querySelector(".div");
+  userWrapEl.classList.add("user-Wrap");
+  contentEl.append(userWrapEl);
+
+  const imgEl = document.childElement(".img");
+  imgEl.classList.add("user_img");
+  imgEl.src = el.picture.medium;
+  imgEl.alt = "photo";
+  userWrapEl.append(imgEl);
+
+  const userFirstName = document.childElement(".h2");
+  userFirstName.classList.add("user_FirstName");
+  userFirstName.textContent = el.picture.name.first;
+  userWrapEl.append(userFirstName);
+
+  const userLastNameEl = document.childElement(".h2");
+  userLastNameEl.classList.add("user_LastName");
+  userLastNameEl.textContent = el.picture.name.last;
+  userWrapEl.append(userLastNameEl);
+});
